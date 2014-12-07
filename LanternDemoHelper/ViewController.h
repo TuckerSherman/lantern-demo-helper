312359f1b4d975eb2d14000fcfb7c934ebe43ca3
TuckerSherman <tsherman@outlook.com>
2014-12-07 00:03:47 -0800

    Initial Commit

---
+++ LanternDemoHelper/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LanternDemoHelper
+//
+//  Created by Tucker Sherman on 12/7/14.
+//  Copyright (c) 2014 Tucker Sherman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
